73378ab420588227573e8e6259fdf7be889ec1f2
ChrisMuteb <ibond.chris@gmail.com>
2022-01-24 14:10:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.gson.GsonBuilder
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // todo some comment
+
+        val gson = GsonBuilder().create()
+
+        val util = StringUtility
+    }
+}
